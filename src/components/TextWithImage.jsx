98ece2ea6bebf34513b2e5eab40e39da953ce027
KhurramMunir95 <khurrammunir9522@gmail.com>
2024-05-30 10:16:06 +0500

    View resume button added in about section

--- src/components/TextWithImage.jsx
+++ src/components/TextWithImage.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
-import { Col, Row } from 'react-bootstrap'
+import { Col, Row, Button } from 'react-bootstrap'
+import FancyBox from '../FancyBox'
+import myResume from '../assets/khurram_resume.pdf'
 
-export const TextWithImage = ({ imgSrc, content, justifyContent }) => {
+export const TextWithImage = ({ id, imgSrc, content, justifyContent, alignment }) => {
   return (
-    <section className='section'>
-        <Row className={`justify-content-${justifyContent}`}>
+    <section className='section pt-0' id={id}>
+        <Row className={`justify-content-${justifyContent} align-items-${alignment}`}>
             <Col md={2}>
                 <img src={imgSrc} alt="Syed Khurram Munir" className="img-fluid img-shadow" />
             </Col>
             <Col md={8}>
                 <h2>About Me</h2>
                 <p>{content}</p>
+            </Col>
+            <Col md={6}>
+              <FancyBox>
+                <Button variant='outline-success' data-fancybox data-type="pdf" data-width="2000" data-height="2000" href={myResume}>View Resume</Button>
+              </FancyBox>
             </Col>
         </Row>
     </section>
