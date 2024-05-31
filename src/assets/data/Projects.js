3f96898065a653e3134374e9d1e78e48f425de34
KhurramMunir95 <khurrammunir9522@gmail.com>
2024-05-31 21:36:36 +0500

    Projects seperate file added

---
+++ src/assets/data/Projects.js
@@ -0,0 +1,36 @@
+import cbImage from '../images/projects/cb.png';
+import idaImage from '../images/projects/ida.png';
+import vrImage from '../images/projects/vr.png';
+import wavetel from '../images/projects/wavetel.png';
+
+const projects = [
+    {
+      title: 'Content Bazaar',
+      content: 'You can find anytype of content here',
+      image: cbImage,
+      url: 'https://www.contentcloud.media/'
+    },
+    {
+      title: 'I Design Architecture',
+      content: `IDA offers comprehensive engineering services tailored to meet the needs of diverse industries. 
+      Specializing in innovative solutions, we provide expert consulting, design, and project management for civil, 
+      mechanical, electrical, and software engineering projects.`,
+      image: idaImage,
+      url: 'https://ida.sa/index'
+    },
+    {
+      title: 'Virtual Reality Solutions',
+      content: 'This platform is designed to offer the latest in VR experiences across gaming, education, training, and entertainment.',
+      image: vrImage,
+      url: 'https://khurrammunir95.github.io/VirtualReality/'
+    },
+    {
+      title: 'Wavetel Business',
+      content: `Formed in 2010, Wavetel has grown to become a leading provider of telecom services in the UK and Rest of the world. 
+      We are proud to be trusted by thousands of organizations across the country.`,
+      image: wavetel,
+      url: 'https://wavetelbusiness.co.uk/'
+    }
+]
+
+export default projects;
